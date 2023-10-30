aa27c1c9b2461c7d7a8e55817b85aa051379c445
Walker Neudorff <walkerneudorff@gmail.com>
2023-10-30 17:21:13 -0500

    Completed base level functionalities, moving onto stretch

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+// EVENT --> STATE --> RENDER
+
+//attack, changes variable, show variable change
+
+
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+
+attackArray = [
+    {
+        name: "Arcane Scepter",
+        attackpoints: 12,
+        hitpoints: 14
+    },
+    {
+        name: "Entangle",
+        attackpoints: 23,
+        hitpoints: 9
+    },
+    {
+        name: "Dragon Blade",
+        attackpoints: 38,
+        hitpoints: 47
+    },
+    {
+        name: "Star Fire",
+        attackpoints: 33,
+        hitpoints: 25
+    }
+]
+
+let fungusHP = 100;
+let attackPoints = 100;
 
 function onReady() {
     console.log("Ready to go!")
@@ -14,6 +45,41 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function attack(attack) {
+    fungusHP -= attack.hitpoints;
+    console.log(fungusHP);
+    attackPoints -= attack.attackpoints;
+    console.log(attackPoints);
+    if (fungusHP <= 0){
+        let fungus = document.getElementById("freaky-fungus")
+        fungus.classList.remove("walk");
+        fungus.classList.add("dead");
+        fungusHP = 0
+    }
+    if (attackPoints <= 0){
+        let fungus = document.getElementById("freaky-fungus")
+        fungus.classList.remove("walk");
+        fungus.classList.add("jump");
+        attackPoints = 0
+        // let disabledAttacks = document.getElementsByClassName("attacks")
+        // button.setAttribute("disabled", true);
+        document.getElementById("attack1").setAttribute("disabled", true);
+        document.getElementById("attack2").setAttribute("disabled", true);
+        document.getElementById("attack3").setAttribute("disabled", true);
+        document.getElementById("attack4").setAttribute("disabled", true);
+    }
+    renderValues ()
+    //render shit
+}
+
+function renderValues () {
+    document.getElementById("attack").textContent = '';
+    document.getElementById("enemyHP").textContent = '';
+    document.getElementById("attack").textContent = `${attackPoints} AP`;
+    document.getElementById("enemyHP").textContent = `${fungusHP} HP`;
+
 }
 
 
